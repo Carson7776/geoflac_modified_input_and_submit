ebfa05caa497905c8314ccce05d6c7ad9e093e65
HU MAO RONG <humaorong409@gmail.com>
2025-10-22 15:55:33 +0800

    adding submit.sh and subduction.inp from geoflac_modified_v00_example in cluster

---
+++ geoflac_modified_v00_example/submit.sh
@@ -0,0 +1,39 @@
+# -S /bin/sh
+#PBS -q batch
+#PBS -o stdout.$PBS_JOBID
+#PBS -e stderr.$PBS_JOBID
+#PBS -V
+#PBS -q q25g2
+#PBS -N geoflac_modified_example
+#PBS -l nodes=1:ppn=8
+
+export OMP_NUM_THREADS=8  # same as ppn
+
+cd $PBS_O_WORKDIR
+cp $PBS_NODEFILE nodelist.$PBS_JOBID
+
+D=/home/humaorong/geoflac_modified
+
+### Recording state of the code
+cp $D/src/snapshot.diff .
+
+### Recording starting time
+echo $(date)>logtime.txt
+
+### Execute the model
+$D/src/flac subduction.inp
+#cp _contents.save _contents.rs
+#$D/src/flac subduction2.inp
+
+### Recording ending time
+echo $(date)>>logtime.txt
+
+### Convert model results
+python3 $D/util/flac2vtk.py ./
+python3 $D/util/flacmarker2vtk.py ./
+
+# ~~~~ submit command ~~~~
+# qsub < [script]
+
+
+
